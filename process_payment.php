71ffe8931a3861d20ed0a95bcca5ef9c57136d8e
CHENSHILING <1231200676@mmu.edu.my>
2025-04-07 21:32:18 +0800

    1

--- process_payment.php
+++ process_payment.php
@@ -18,6 +18,7 @@ $email = isset($_SESSION["email"]) ? $conn->real_escape_string($_SESSION["email"
 
 
 // get data
+$class_id = $conn->real_escape_string($data["class_id"]); 
 $student_name = $conn->real_escape_string($data["student_name"]);
 $course_name = $conn->real_escape_string($data["course_name"]);
 $teacher_name = $conn->real_escape_string($data["teacher_name"]);
@@ -26,8 +27,8 @@ $payment_method = $conn->real_escape_string($data["payment_method"]);
 $time = $conn->real_escape_string($data["time"]);
 
 // insert data
-$sql = "INSERT INTO orders (email, student_name, course_name, teacher_name, total_amount, payment_method, time) 
-        VALUES ('$email', '$student_name', '$course_name', '$teacher_name', '$total_amount', '$payment_method', '$time')";
+$sql = "INSERT INTO orders (class_id, email, student_name, course_name, teacher_name, total_amount, payment_method, time) 
+        VALUES ('$class_id','$email', '$student_name', '$course_name', '$teacher_name', '$total_amount', '$payment_method', '$time')";
 
 if ($conn->query($sql) === TRUE) {
     $order_id = $conn->insert_id;
