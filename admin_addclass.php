1ed4e706cc693a0138048fb1bc5fca333a5d1b37
CHENSHILING <1231200676@mmu.edu.my>
2025-04-10 22:04:09 +0800

    1

--- admin_addclass.php
+++ admin_addclass.php
@@ -18,14 +18,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $subjectid = $_POST["subjectid"];
     $classid = $_POST["classid"];
     $year = $_POST["year"];
-    $day = $_POST["day"];
+    $part = $_POST["part"];
+    $month = $_POST["month"];
     $time = $_POST["time"];
     $teacher = $_POST["teacher"];
     $enrollment = $_POST["enrollment"];
+    $status = $_POST["status"];
 
    
-    $sql = "INSERT INTO admin_class (subject_id, class_id, year, day, time, teacher, capacity) 
-            VALUES ('$subjectid', '$classid', '$year', '$day', '$time', '$teacher', '$enrollment')";
+$sql = "INSERT INTO admin_class (subject_id, class_id, year, part, month, time, teacher, capacity, status) 
+        VALUES ('$subjectid', '$classid', '$year', '$part', '$month', '$time', '$teacher', '$enrollment', '$status')";
 
     if ($conn->query($sql) === TRUE) {
         echo "<script>alert('Class added successfully!'); window.location.href='admin class.php';</script>";
