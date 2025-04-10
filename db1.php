24e97e1a3a3d51fd582b2472d107594bff365340
LIMENXI <1231200870@student.mmu.edu.my>
2025-04-10 23:53:42 +0800

    1

--- db1.php
+++ db1.php
@@ -1,4 +1,6 @@
 <?php
 $db = new mysqli("localhost", "root", "", "admin");
-if($db->connect_error) die("连接失败");
+if ($db->connect_error) {
+    die("连接失败: " . $db->connect_error);  // 输出详细的错误信息
+}
 ?>
