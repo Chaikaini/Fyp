086a59cc66271c5363d88d30446302b969679510
Chaikaini <1231201291@mmu.edu.my>
2025-04-13 21:01:40 +0800

    1

--- get_comments.php
+++ get_comments.php
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT year, subject, rating, comment, created_at FROM comments ORDER BY created_at DESC";
+$sql = "SELECT comment_year, comment_subject, comment_rating, comment_text, comment_created_at FROM comments ORDER BY comment_created_at DESC";
 $result = $conn->query($sql);
 
 $comments = [];
