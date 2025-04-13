d65c159bec697234734ed1811f3280ce3339a74a
CHENSHILING <1231200676@mmu.edu.my>
2025-04-13 14:23:45 +0800

    1

---
+++ sql/the_seeds.sql
@@ -0,0 +1,392 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 13, 2025 at 08:21 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `the seeds`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `admin_id` int(11) NOT NULL,
+  `admin_name` varchar(100) NOT NULL,
+  `admin_gender` enum('Male','Female') NOT NULL,
+  `admin_email` varchar(100) NOT NULL,
+  `admin_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `attendance_id` int(11) NOT NULL,
+  `registration_id` int(11) NOT NULL,
+  `attendance_name` varchar(100) NOT NULL,
+  `attendance_day` date NOT NULL,
+  `attendance_result` enum('Present','Absent','Late') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `child`
+--
+
+CREATE TABLE `child` (
+  `child_id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `child_name` varchar(100) NOT NULL,
+  `child_gender` enum('Male','Female','Other') NOT NULL,
+  `child_kidNumber` varchar(50) NOT NULL,
+  `child_birthday` date NOT NULL,
+  `child_school` varchar(100) NOT NULL,
+  `child_year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `class_id` varchar(20) NOT NULL,
+  `subject_id` varchar(20) NOT NULL,
+  `part_id` int(11) NOT NULL,
+  `year` year(4) NOT NULL,
+  `class_time` time NOT NULL,
+  `class_capacity` int(11) NOT NULL,
+  `class_status` enum('Available','Unavailable','Full') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `notification_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `class_id` varchar(20) NOT NULL,
+  `notification_category` varchar(100) NOT NULL,
+  `notification_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `order_id` int(11) NOT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
+  `payment_method` enum('Credit Card','Cash','Bank Transfer','Other') NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `registration_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parent`
+--
+
+CREATE TABLE `parent` (
+  `parent_id` int(11) NOT NULL,
+  `parent_name` varchar(100) NOT NULL,
+  `ic_number` varchar(20) NOT NULL,
+  `parent_email` varchar(100) NOT NULL,
+  `parent_address` text NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `phone_number2` varchar(20) DEFAULT NULL,
+  `parent_relationship` enum('Parent','Guardian') NOT NULL,
+  `parent_gender` enum('Male','Female') NOT NULL,
+  `parent_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `part`
+--
+
+CREATE TABLE `part` (
+  `part_id` int(11) NOT NULL,
+  `part_name` varchar(100) NOT NULL,
+  `part_duration` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration_class`
+--
+
+CREATE TABLE `registration_class` (
+  `registration_id` int(11) NOT NULL,
+  `parent_id` int(11) NOT NULL,
+  `class_id` varchar(20) NOT NULL,
+  `child_id` int(11) NOT NULL,
+  `subject_id` varchar(20) NOT NULL,
+  `teacher_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `subject_id` varchar(20) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `subject_name` varchar(100) NOT NULL,
+  `subject_price` decimal(8,2) NOT NULL,
+  `subject_description` text DEFAULT NULL,
+  `subject_image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teacher`
+--
+
+CREATE TABLE `teacher` (
+  `teacher_id` int(11) NOT NULL,
+  `teacher_name` varchar(100) NOT NULL,
+  `teacher_gender` enum('Male','Female') NOT NULL,
+  `teacher_email` varchar(100) NOT NULL,
+  `teacher_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `admin_email` (`admin_email`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`attendance_id`),
+  ADD KEY `registration_id` (`registration_id`);
+
+--
+-- Indexes for table `child`
+--
+ALTER TABLE `child`
+  ADD PRIMARY KEY (`child_id`),
+  ADD UNIQUE KEY `child_kidNumber` (`child_kidNumber`),
+  ADD KEY `parent_id` (`parent_id`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`class_id`),
+  ADD KEY `subject_id` (`subject_id`),
+  ADD KEY `part_id` (`part_id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`notification_id`),
+  ADD KEY `admin_id` (`admin_id`),
+  ADD KEY `class_id` (`class_id`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `registration_id` (`registration_id`);
+
+--
+-- Indexes for table `parent`
+--
+ALTER TABLE `parent`
+  ADD PRIMARY KEY (`parent_id`),
+  ADD UNIQUE KEY `ic_number` (`ic_number`),
+  ADD UNIQUE KEY `parent_email` (`parent_email`);
+
+--
+-- Indexes for table `part`
+--
+ALTER TABLE `part`
+  ADD PRIMARY KEY (`part_id`);
+
+--
+-- Indexes for table `registration_class`
+--
+ALTER TABLE `registration_class`
+  ADD PRIMARY KEY (`registration_id`),
+  ADD KEY `parent_id` (`parent_id`),
+  ADD KEY `class_id` (`class_id`),
+  ADD KEY `child_id` (`child_id`),
+  ADD KEY `subject_id` (`subject_id`),
+  ADD KEY `teacher_id` (`teacher_id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`subject_id`),
+  ADD KEY `teacher_id` (`teacher_id`),
+  ADD KEY `admin_id` (`admin_id`);
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`teacher_id`),
+  ADD UNIQUE KEY `teacher_email` (`teacher_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `child`
+--
+ALTER TABLE `child`
+  MODIFY `child_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `notification_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `parent`
+--
+ALTER TABLE `parent`
+  MODIFY `parent_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `part`
+--
+ALTER TABLE `part`
+  MODIFY `part_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registration_class`
+--
+ALTER TABLE `registration_class`
+  MODIFY `registration_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+  MODIFY `teacher_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`registration_id`) REFERENCES `registration_class` (`registration_id`);
+
+--
+-- Constraints for table `child`
+--
+ALTER TABLE `child`
+  ADD CONSTRAINT `child_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `parent` (`parent_id`);
+
+--
+-- Constraints for table `class`
+--
+ALTER TABLE `class`
+  ADD CONSTRAINT `class_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`),
+  ADD CONSTRAINT `class_ibfk_2` FOREIGN KEY (`part_id`) REFERENCES `part` (`part_id`);
+
+--
+-- Constraints for table `notification`
+--
+ALTER TABLE `notification`
+  ADD CONSTRAINT `notification_ibfk_1` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`),
+  ADD CONSTRAINT `notification_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`);
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`registration_id`) REFERENCES `registration_class` (`registration_id`);
+
+--
+-- Constraints for table `registration_class`
+--
+ALTER TABLE `registration_class`
+  ADD CONSTRAINT `registration_class_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `parent` (`parent_id`),
+  ADD CONSTRAINT `registration_class_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`),
+  ADD CONSTRAINT `registration_class_ibfk_3` FOREIGN KEY (`child_id`) REFERENCES `child` (`child_id`),
+  ADD CONSTRAINT `registration_class_ibfk_4` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`),
+  ADD CONSTRAINT `registration_class_ibfk_5` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`);
+
+--
+-- Constraints for table `subject`
+--
+ALTER TABLE `subject`
+  ADD CONSTRAINT `subject_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `teacher` (`teacher_id`),
+  ADD CONSTRAINT `subject_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `admin` (`admin_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
