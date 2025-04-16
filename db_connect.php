ecad24cd0ca4ac1a69460d65f2a9e4621e33f369
LIMENXI <1231200870@student.mmu.edu.my>
2025-04-17 00:01:40 +0800

    1

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "the_seeds";
+$dbname = "the seeds";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
 }
 
 function dbConnect() {
-    $conn = new mysqli("localhost", "root", "", "the_seeds");
+    $conn = new mysqli("localhost", "root", "", "the seeds");
     if ($conn->connect_error) {
         die(json_encode(['status' => 'error', 'message' => 'Database connection failed']));
     }
