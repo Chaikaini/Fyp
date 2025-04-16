f42b533880387a16cdadef54bd7425f3afccc2da
LIMENXI <1231200870@student.mmu.edu.my>
2025-04-16 23:59:38 +0800

    1

--- db_connect.php
+++ db_connect.php
@@ -1,21 +1,19 @@
 <?php
 $servername = "localhost";
-$username = "root"; // 数据库用户名
-$password = ""; // 数据库密码
-$dbname = "tuition_centre"; // 数据库名称
+$username = "root";
+$password = "";
+$dbname = "the_seeds";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    die("database connection fail: " . $conn->connect_error);
+    die("Database connection failed: " . $conn->connect_error);
 }
 
 function dbConnect() {
-    $conn = new mysqli("localhost", "root", "", "tuition_centre");
-
+    $conn = new mysqli("localhost", "root", "", "the_seeds");
     if ($conn->connect_error) {
         die(json_encode(['status' => 'error', 'message' => 'Database connection failed']));
     }
-
     return $conn;
 }
 ?>
