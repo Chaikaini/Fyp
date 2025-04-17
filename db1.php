7d2053c3641d97561f0c84cd5da4e3c2e743b662
LIMENXI <1231200870@student.mmu.edu.my>
2025-04-17 23:34:50 +0800

    1

--- db1.php
+++ db1.php
@@ -4,7 +4,7 @@
 $db_host = 'localhost';  // 数据库主机
 $db_user = 'root';       // 数据库用户名
 $db_pass = '';           // 数据库密码
-$db_name = 'admin';      // 数据库名
+$db_name = 'the seeds';      // 数据库名
 
 // 创建数据库连接
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
