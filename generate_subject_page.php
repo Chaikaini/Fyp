c162295d213898111c7c34f7a8774c6b0af954c6
LIMENXI <enxi6387@gmail.com>
2025-05-24 15:57:26 +0800

    1

--- generate_subject_page.php
+++ generate_subject_page.php
@@ -505,29 +505,29 @@ unset($_SESSION['message']);
             <div id="review-list"></div>
         </div>
     </div>
+     <!-- Footer Start -->
     <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
+                <!-- 品牌标语 -->
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-white mb-4">The Seeds Learning Tuition Centre</h4>
+                    <p>Every child is a different kind of flower. We nurture their growth.</p>
+                </div>
+                <!-- Quick Link -->
+                <div class="col-lg-4 col-md-6">
                     <h4 class="text-white mb-4">Quick Link</h4>
-                    <a class="btn btn-link" href="about.php">About Us</a>
-                    <a class="btn btn-link" href="contact.php">Contact Us</a>
+                    <a class="btn btn-link" href="about.html">About Us</a><br>
                 </div>
-                <div class="col-lg-3 col-md-6">
+                <!-- Contact -->
+                <div class="col-lg-4 col-md-6">
                     <h4 class="text-white mb-4">Contact</h4>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>5262A, Jalan Matahari, Bandar Indahpura, 81000 Kulai, Johor Darul Ta'zim</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+011 775 8990</p>
                     <p class="mb-2"><i class="fa fa-envelope me-3"></i>TheSeeds@gmail.com</p>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h4 class="text-white mb-4">Gallery</h4>
-                    <div class="row g-2 pt-2">
-                        <div class="col-4"><img class="img-fluid bg-light p-1" src="img/course-1.jpg" alt=""></div>
-                        <div class="col-4"><img class="img-fluid bg-light p-1" src="img/course-2.jpg" alt=""></div>
-                        <div class="col-4"><img class="img-fluid bg-light p-1" src="img/course-3.jpg" alt=""></div>
-                        <div class="col-4"><img class="img-fluid bg-light p-1" src="img/course-2.jpg" alt=""></div>
-                        <div class="col-4"><img class="img-fluid bg-light p-1" src="img/course-3.jpg" alt=""></div>
-                        <div class="col-4"><img class="img-fluid bg-light p-1" src="img/course-1.jpg" alt=""></div>
+                    <!-- 社交媒体（可替换为真实链接） -->
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href="https://www.facebook.com/people/The-Seeds-Learning-Centre/100063525220441/#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://www.instagram.com/theseeds_kulai?igsh=dGt4YWpiOWJiem44"><i class="fab fa-instagram"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://wa.me/60117758990"><i class="fab fa-whatsapp"></i></a>
                     </div>
                 </div>
             </div>
@@ -536,12 +536,16 @@ unset($_SESSION['message']);
             <div class="copyright">
                 <div class="row">
                     <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        © <a class="border-bottom" href="#">The Seeds Learning Centre</a>, All Rights Reserved.
+                        © <a class="border-bottom" href="#">The Seeds Learning Tuition Centre</a>, All Right Reserved.
+                    </div>
+                    <div class="col-md-6 text-center text-md-end">
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <!-- Footer End -->
+     
     <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
